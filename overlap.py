098eee815517c3c942f9298a6738524c16d828e8
Muhammad Afzaal <38922111+AfzaalQader@users.noreply.github.com>
2020-03-01 01:22:25 +0500

    change logic

--- overlap.py
+++ overlap.py
@@ -1,42 +1,40 @@
-pointX1 = int(input('Please enter X coordinate of 1st rectangle:	'))
-pointY1 = int(input('Please enter Y coordinate of 1st rectangle:	'))
+x1 = int(input('Please enter X coordinate of 1st rectangle:	'))
+y1 = int(input('Please enter Y coordinate of 1st rectangle:	'))
 width1 = int(input('Please enter width of 1st rectangle:	'))
 height1 = int(input('Please enter height of 1st rectangle:	'))
 
-pointX2 = int(input('Please enter X coordinate of 2nd rectangle:	'))
-pointY2 = int(input('Please enter Y coordinate of 2nd rectangle:	'))
+x2 = int(input('Please enter X coordinate of 2nd rectangle:	'))
+y2 = int(input('Please enter Y coordinate of 2nd rectangle:	'))
 width2 = int(input('Please enter width of 2nd rectangle:	'))
 height2 = int(input('Please enter height of 2nd rectangle:	'))
 
 #Left uper corner coerdinnate A(pointX1,pointY1)
-#Left bottom corner coordinate B(pointX1,height1)
-#Right uper corner coordinate C(width1,pointY1)
-#Right bottom corner coordinate D(width1,height1)
+#Left bottom corner coordinate B(pointX1,pointY1 + height1)
+#Right uper corner coordinate C(pointX1 + width1,pointY1)
+#Right bottom corner coordinate D(pointX1 + width1, pointY1 + height1)
 #Euqtions of first rectangle 
 #top
 
 def point_check(pointX,pointY):
-	"""This will check the point weather this point within a rectangle or not here PointY1,height1,PointX1 and width1 is constant"""
-	eq1 = pointY - pointY1
-	eq2 = pointY - height1
-	eq3 = pointX - pointX1
-	eq4 = pointX - width1
-	if((eq1 <= 0 and eq2 >= 0) and (eq3 >= 0 and eq4 <= 0)):
+	if((pointX >= x1 and pointX <= x1 + width1) and (pointY >= y1 and pointY <= y1 + height1)):
 		return False
 	else:
 		return True
 
 #Starting point recctangle 2 is (pointX2,pointY2)
-#Left bottom corner coordinate (pointX2,height2)
-#Right uper corner coordinate (width2,pointY2)
-#Right bottom corner coordinate (width2,height2)
+#Left bottom corner coordinate (pointX2,pointY2 + height2)
+#Right uper corner coordinate (pointX2 + width2,pointY2)
+#Right bottom corner coordinate (pointX1 + width2,pointY2 + height2)
 
-top_left = point_check(pointX2,pointY2)
-top_right = point_check(width2,pointY2)
-bottom_left = point_check(pointX2,height2)
-bottom_right = point_check(width2,height2)
-
-if(top_left == True or top_right == True or bottom_left == True or bottom_right == True):
-	print('Both Rectangle are overlaped')
+top_left = point_check(x2,y2)
+print(top_left)
+top_right = point_check(x2 + width2,y2)
+print(top_right)
+bottom_left = point_check(x2,y2 + height2)
+print(bottom_left)
+bottom_right = point_check(x2 + width2,y2 + height2)
+print(bottom_right)
+if(top_left == False or top_right == False or bottom_left == False or bottom_right == False):
+	print('Both Rectangles are overlaped')
 else:
-	print('Both Rectangle are not overlaped')
+	print('Both Rectangles are not overlaped')
